d04079fcd3a8a33c32a9292e6875e9391b658206
Mick Gortenmulder <mick.gortenmulder@sentia.com>
2018-09-21 18:03:16 +0200

    third iteration

--- analyze.rb
+++ analyze.rb
@@ -14,7 +14,7 @@ class MoodAnalyzer
   def convert_videos
     converted_hash = Hash.new { |hash,key| hash[key] = Hash.new }
 
-    @videos2.each do |video|
+    @videos2.first(10).each do |video|
       name = video["Title"]
 
       # print
@@ -27,12 +27,10 @@ class MoodAnalyzer
 
       next if video["CommentList"].nil?
 
-      converted_hash[name][:description] = video["Description"]
       converted_hash[name][:link] = video["VideoUrl"]
-      converted_hash[name][:input] = video["CommentList"][0..9]
-      # converted_hash[name][:mood] = emotion(subhash[:comments].join(","))
-      converted_hash[name][:mood] = "test"
-      converted_hash[name][:converted_time] = Time.now
+      # converted_hash[name][:input] = video["CommentList"].first(5)
+      converted_hash[name][:mood] = emotion(video["CommentList"].first(5))
+      # converted_hash[name][:mood] = "test"
     end
 
     converted_hash
@@ -44,7 +42,7 @@ class MoodAnalyzer
     mood_hash.each do |video_name,video_info|
       puts video_name
       puts video_info
-      @client.index index: 'mood-analyzer', type: 'my-video-mood', id: 1, body: { title: video_name, description: video_info[:description], link: video_info[:link], input: video_info[:input], mood: video_info[:mood], converted_time: video_info[:converted_time] }
+      @client.index index: 'mood-analyzer', type: 'my-video-mood', body: { title: video_name, link: video_info[:link], mood: video_info[:mood] }
     end
   end
 end
